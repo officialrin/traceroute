b8bfa9d8e16e717ce2fbb4ae3e079293ada70413
rin <75649277+officialrin@users.noreply.github.com>
2021-12-11 02:41:42 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -79,7 +79,7 @@ def get_route(hostname):
 
     for ttl in range(1, MAX_HOPS):
         for tries in range(TRIES):
-            destAddr = gethostbyname(hostname)
+            destAddr = gethostbyname(str(hostname))
 
             # Fill in start
             icmp = getprotobyname("icmp")
