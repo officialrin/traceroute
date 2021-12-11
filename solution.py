c04d4143c966bbe94ff8368683267a23992183b5
rin <75649277+officialrin@users.noreply.github.com>
2021-12-10 23:29:17 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -52,7 +52,7 @@ def build_packet():
     myID = os.getpid() & 0xFFFF
     # Make a dummy header with a 0 checksum
     # struct -- Interpret strings as packed binary data
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1)
     data = struct.pack("d", time.time())
     # Calculate the checksum on the data and the dummy header.
     myChecksum = checksum(header + data)
